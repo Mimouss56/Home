2be6204e7b6eebda43fd02a1266c30e35a8edfc0
Matthieu LP <lepriol.matthieu@gmail.com>
2023-08-28 10:47:49 +0200

    update ProtectedRoute && JWT verify

--- src/components/ProtectedRoute/index.tsx
+++ src/components/ProtectedRoute/index.tsx
@@ -7,10 +7,26 @@ interface ProtectedRouteProps {
 // si non redirection vers formulaire de connexion
 
 function ProtectedRoute({ children }: ProtectedRouteProps) {
-  const token = sessionStorage.getItem('sessionToken');
-  console.log(token);
+  const token = sessionStorage.getItem('sessionToken') || '';
 
-  return sessionStorage.getItem('sessionToken') ? children : <Navigate to="/" replace />;
+  // Si absence de Token dans le sessionStorage, redirection vers la page d'accueil
+  if (!token) {
+    sessionStorage.setItem('notifToast', 'Vous devez être connecté pour accéder à cette page');
+    return <Navigate to="/" replace />;
+  }
+  // Si Token présent dans le sessionStorage, vérification de sa validité
+
+  // On récupère la date d'expiration du token
+  const tokenExp = JSON.parse(atob(token.split('.')[1])).exp;
+  const dateExp = new Date(tokenExp).valueOf();
+  const dateNow = Date.now().valueOf() / 1000;
+
+  if (dateNow > dateExp) {
+    sessionStorage.removeItem('sessionToken');
+    sessionStorage.setItem('notifToast', 'Votre session a expiré');
+    return <Navigate to="/" replace />;
+  }
+  return children;
 }
 
 export default ProtectedRoute;
