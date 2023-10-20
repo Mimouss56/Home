07eae1cc842671980ba7d23d3d5926ff443b7c09
Mouss <lepriol.matthieu@gmail.com>
2023-10-20 23:08:36 +0200

    Change AuthorSanctionID SQL

--- app/base_BBC/services/user.service.js
+++ app/base_BBC/services/user.service.js
@@ -7,80 +7,103 @@ discord.SetAccessInfo('Bot', tokenBot);
 
 module.exports = {
   async getData(idUser) {
-    const guildRoles = await discord.Api('GET', `/guilds/${guildID}/roles`);
-
+    // 1. Récupérez d'abord les informations de userInfos.
     const userInfos = await User.findByPk(idUser);
-    const donateurInfos = await donator.findByPk(idUser);
 
+    if (!userInfos) {
+      // Gérez l'erreur si userInfos n'existe pas. Par exemple, retournez null ou lancez une erreur.
+      return { error: 'User not found' };
+    }
+
+    // 2. Utilisez le discord_id de userInfos pour obtenir les informations de userDiscord.
+    const guildRoles = await discord.Api('GET', `/guilds/${guildID}/roles`);
     const userDiscord = await discord.Api('GET', `/guilds/${guildID}/members/${userInfos.discord_id}`);
 
+    // 3. Enrichissez les informations userInfos avec celles de userDiscord.
     const userReturn = {
-      username: userDiscord.user.username,
-      discord_id: userDiscord.user.id,
-      locale: userDiscord.user.locale,
-      url_avatar: `https://cdn.discordapp.com/avatars/${userDiscord.user.id}/${userDiscord.user.avatar}.png`,
-      status: userDiscord.roles.includes(masterRole),
-      roles: guildRoles.filter((role) => userDiscord.roles.includes(role.id)).map((role) => ({
-        id: role.id,
-        name: role.name,
-        color: `#${role.color.toString(16).padStart(6, '0')}`,
-      })),
+      // Propriétés de userInfos
       id: userInfos.id,
       nom: userInfos.nom,
       prenom: userInfos.prenom,
       email: userInfos.email,
       is_admin: userInfos.is_admin,
-    };
-    // if donateurInfos not null add donateurInfos and donUser to userReturn
-    if (donateurInfos) {
-      userReturn.is_donator = donateurInfos;
-      userReturn.dons = await don.findAll({ where: { user_id: idUser } });
-      // userReturn.solde = await this.countSolde(idUser);
-    }
-
-    return userReturn;
-  },
-
-  async getAll() {
-    const guildRoles = await discord.Api('GET', `/guilds/${guildID}/roles`);
-    const listUserDiscord = await discord.Api('GET', `/guilds/${guildID}/members?limit=1000`);
-
-    // Mapper chaque utilisateur Discord à l'objet désiré
-    const usersReturn = await Promise.all(listUserDiscord.map(async (userDiscord) => {
-      const userInfos = await User.findOne({ where: { discord_id: Number(userDiscord.user.id) } });
-
-      const userReturn = {
+      discordInfos: {
         username: userDiscord.nick || userDiscord.user.global_name || userDiscord.user.username,
         discord_id: userDiscord.user.id,
         locale: userDiscord.user.locale,
         url_avatar: `https://cdn.discordapp.com/avatars/${userDiscord.user.id}/${userDiscord.user.avatar}.png`,
-        status: userDiscord.roles.includes(masterRole),
         roles: guildRoles.filter((role) => userDiscord.roles.includes(role.id)).map((role) => ({
           id: role.id,
           name: role.name,
           color: `#${role.color.toString(16).padStart(6, '0')}`,
         })),
-      };
-      if (userInfos) {
-        const donateurInfos = await donator.findByPk(userInfos.id);
-        userReturn.siteBBC = {
+      },
+    };
+    // Si donateurInfos existe, ajoutez ces informations à userReturn.
+    const donateurInfos = await donator.findByPk(idUser);
+    if (donateurInfos) {
+      userReturn.is_donator = donateurInfos;
+      userReturn.dons = await don.findAll({ where: { user_id: idUser } });
+    }
+    return userReturn;
+  },
 
-          id: userInfos.id,
-          nom: userInfos.nom,
-          prenom: userInfos.prenom,
-          email: userInfos.email,
-          is_admin: userInfos.is_admin,
+  async getAll() {
+    // 1. Récupérez tous les utilisateurs de votre base de données.
+    const allUsers = await User.findAll();
+
+    // 2. Récupérez les informations du serveur Discord.
+    const guildRoles = await discord.Api('GET', `/guilds/${guildID}/roles`);
+    const listUserDiscord = await discord.Api('GET', `/guilds/${guildID}/members?limit=1000`);
+
+    // 3. Mappez les utilisateurs pour enrichir les informations avec celles de listUserDiscord.
+    const usersReturn = await Promise.all(allUsers.map(async (userInfo) => {
+      // Trouvez le membre Discord correspondant pour cet utilisateur.
+      const userDiscord = listUserDiscord.find((ud) => ud.user.id === String(userInfo.discord_id));
+
+      let discordInfos;
+      if (userDiscord) {
+        discordInfos = {
+          username: userDiscord.nick || userDiscord.user.global_name || userDiscord.user.username,
+          discord_id: userDiscord.user.id,
+          locale: userDiscord.user.locale,
+          url_avatar: `https://cdn.discordapp.com/avatars/${userDiscord.user.id}/${userDiscord.user.avatar}.png`,
+          status: userDiscord.roles.includes(masterRole),
+          roles: guildRoles.filter((role) => userDiscord.roles.includes(role.id)).map((role) => ({
+            id: role.id,
+            name: role.name,
+            color: `#${role.color.toString(16).padStart(6, '0')}`,
+          })),
         };
-        if (donateurInfos) {
-          userReturn.is_donator = donateurInfos;
-          userReturn.dons = await don.findAll({ where: { user_id: userInfos.id } });
-          // userReturn.solde = await this.countSolde(idUser);
-        }
+      } else {
+        discordInfos = 'User Deleted';
       }
 
-      return userReturn;
+      return {
+        // Propriétés de userInfo
+        id: userInfo.id,
+        nom: userInfo.nom,
+        prenom: userInfo.prenom,
+        email: userInfo.email,
+        is_admin: userInfo.is_admin,
+        discordInfos,
+
+        // Si vous voulez inclure donateurInfos
+        ...(await this.getDonatorInfo(userInfo.id)),
+      };
     }));
 
     return usersReturn;
+  },
+  // Cette fonction helper peut être utilisée pour obtenir des informations sur le donateur.
+  async getDonatorInfo(idUser) {
+    const donateurInfos = await donator.findByPk(idUser);
+    if (!donateurInfos) return {};
+
+    return {
+      is_donator: donateurInfos,
+      dons: await don.findAll({ where: { user_id: idUser } }),
+      // solde: await this.countSolde(idUser);  // Si vous voulez inclure cette logique.
+    };
   },
 };
