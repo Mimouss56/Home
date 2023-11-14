b4ef18e7d2ccdf571aaf17c0c823455d781b5c10
Mouss <lepriol.matthieu@gmail.com>
2023-11-14 12:20:37 +0100

    update view Login Register + suppress antd

--- src/Pages/Auth/login.tsx
+++ src/Pages/Auth/login.tsx
@@ -1,20 +1,21 @@
 import { useState } from 'react';
-import {
-  Form, Input, Button,
-} from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import { LoginPost } from '../../@types/login';
 import axiosInstance from '../../utils/axios';
 
 function Login() {
-  const [form] = Form.useForm();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
-  const handleSubmit = async (values: LoginPost) => {
+  const handleSubmit = async () => {
     try {
-      const res = await axiosInstance.post('/home/login', values);
+      setLoading(true);
+      setError(false);
+      setErrorMessage('');
+
+      const res = await axiosInstance.post('/home/login', { username, password });
       const { data, sessionToken, message } = res.data;
 
       sessionStorage.setItem('sessionToken', sessionToken);
@@ -31,77 +32,65 @@ function Login() {
   };
 
   return (
-    <Form
-      form={form}
-      name="login"
-      initialValues={{ remember: false }}
-      onFinish={(values) => {
-        setLoading(true);
-        setError(false);
-        setErrorMessage('');
-        handleSubmit(values);
-      }}
-    >
-      <div
-        className="modal fade"
-        id="modalLogin"
-        tabIndex={-1}
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="exampleModalLabel">Connexion</h1>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+    <div className="modal fade" id="modalLogin" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5" id="exampleModalLabel">Connexion</h1>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+          </div>
+          <div className="modal-body">
+            <div className={`input-group mb-3 ${error ? 'has-error' : ''}`}>
+              <i className="input-group-text bi bi-person" />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              {error && <span className="help-block">{errorMessage}</span>}
             </div>
-            <div className="modal-body">
+            <div className={`input-group mb-3 ${error ? 'has-error' : ''}`}>
+              <i className="input-group-text bi bi-key" />
+              <input
+                type={showPassword ? 'text' : 'password'}
+                className="form-control"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button
+                type="button"
+                className={`input-group-text bi bi-eye${showPassword ? '-slash' : ''}`}
+                onClick={() => setShowPassword(!showPassword)}
+              />
 
-              <Form.Item
-                name="username"
-                rules={[{ required: true, message: 'Please input your username!' }]}
-                validateStatus={error ? 'error' : 'success'}
-                help={errorMessage}
-              >
-                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-              </Form.Item>
-              <Form.Item
-                name="password"
-                rules={[{ required: true, message: 'Please input your password!' }]}
-                validateStatus={error ? 'error' : 'success'}
-              >
-                <Input
-                  prefix={<LockOutlined className="site-form-item-icon" />}
-                  type="password"
-                  placeholder="Password"
-                />
-              </Form.Item>
-              <Form.Item />
+              {error && <span className="help-block">{errorMessage}</span>}
             </div>
-            <div className="modal-footer d-flex justify-content-around">
-              <Button
-                type="primary"
-                htmlType="button"
-                className="btn btn-secondary"
-                data-bs-target="#modalregister"
-                data-bs-toggle="modal"
-                data-bs-dismiss="modal"
-              >
-                Register
-              </Button>
-              <Button
-                type="primary"
-                htmlType="submit"
-                className="btn btn-primary"
-                loading={loading}
-              >
-                Log in
-              </Button>
-            </div>
+          </div>
+          <div className="modal-footer d-flex justify-content-around">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-target="#modalregister"
+              data-bs-toggle="modal"
+              data-bs-dismiss="modal"
+            >
+              Register
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={handleSubmit}
+              disabled={loading}
+            >
+              Log in
+            </button>
           </div>
         </div>
       </div>
-    </Form>
+    </div>
   );
 }
 
--- src/Pages/Auth/register.tsx
+++ src/Pages/Auth/register.tsx
@@ -1,36 +1,36 @@
-/* eslint-disable react/no-unstable-nested-components */
 import { useState } from 'react';
-import {
-  Form, Input, Button, Alert,
-} from 'antd';
-import {
-  UserOutlined,
-  LockOutlined,
-  EyeTwoTone,
-  EyeInvisibleOutlined,
-} from '@ant-design/icons';
-import { RegisterPost } from '../../@types/Home/register';
 import axiosInstance from '../../utils/axios';
 
 function Register() {
-  const [form] = Form.useForm();
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const [success, setSuccess] = useState(false);
-
-  const handleSubmit = async (values: RegisterPost) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const handleSubmit = async () => {
     const dataInput = {
-      username: values.username,
-      password: values.password,
-      confirmPassword: values.confirmPassword,
-      email: values.email,
+      username,
+      password,
+      confirmPassword,
+      email,
     };
+
     try {
+      setLoading(true);
+      setError(false);
+      setErrorMessage('');
+      setSuccess(false);
+
       const res = await axiosInstance.post('/home/register', dataInput);
       sessionStorage.setItem('notifToast', `${res.data.message}, Merci de vous reconnecter !`);
 
-      window.location.reload();
+      setSuccess(true);
+      setLoading(false);
     } catch (err) {
       const { response } = err as any;
       setError(true);
@@ -40,101 +40,98 @@ function Register() {
   };
 
   return (
-    <Form
-      form={form}
-      name="register"
-      initialValues={{ remember: true }}
-      onFinish={(values) => {
-        setLoading(true);
-        setError(false);
-        setErrorMessage('');
-        setSuccess(false);
-        handleSubmit(values);
-      }}
-    >
-      <div
-        className="modal fade"
-        id="modalregister"
-        tabIndex={-1}
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="exampleModalLabel">S&apos;enregistrer</h1>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+    <div className="modal fade" id="modalregister" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5" id="exampleModalLabel">S&apos;enregistrer</h1>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+          </div>
+          <div className="modal-body">
+            <div className={`input-group mb-3 ${error ? 'has-error' : ''}`}>
+              <i className="input-group-text bi bi-person" />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              {error && <span className="help-block">{errorMessage}</span>}
             </div>
-            <div className="modal-body">
-              <Form.Item
-                name="username"
-                rules={[{ required: true, message: 'Please input your username!' }]}
-                validateStatus={error ? 'error' : 'success'}
-                help={errorMessage}
-              >
-                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-              </Form.Item>
-              <Form.Item
-                name="email"
-                rules={[{ required: true, message: 'Please input your email!' }]}
-                validateStatus={error ? 'error' : 'success'}
-              >
-                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
-              </Form.Item>
-
-              <Form.Item
-                name="password"
-                rules={[{ required: true, message: 'Please input your password!' }]}
-                validateStatus={error ? 'error' : 'success'}
-              >
-                <Input.Password
-                  prefix={<LockOutlined className="site-form-item-icon" />}
-                  placeholder="Password"
-                />
-              </Form.Item>
-              <Form.Item
-                name="confirmPassword"
-                rules={[{ required: true, message: 'Please confirm your password!' }]}
-                validateStatus={error ? 'error' : 'success'}
-              >
-                <Input.Password
-                  prefix={<LockOutlined className="site-form-item-icon" />}
-                  placeholder="Confirm Password"
-                  iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-                />
-              </Form.Item>
-              {success && (
-                <Form.Item>
-                  <Alert message="Login success" type="success" showIcon />
-                </Form.Item>
-              )}
-
+            <div className={`input-group mb-3 ${error ? 'has-error' : ''}`}>
+              <i className="input-group-text">@</i>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              {error && <span className="help-block">{errorMessage}</span>}
             </div>
-            <div className="modal-footer d-flex justify-content-around">
-              <Button
-                type="primary"
-                htmlType="button"
-                className="btn btn-secondary"
-                data-bs-target="#modalLogin"
-                data-bs-toggle="modal"
-                data-bs-dismiss="modal"
-              >
-                Login
-              </Button>
-
-              <Button
-                type="primary"
-                htmlType="submit"
-                className="btn btn-primary"
-                loading={loading}
-              >
-                Sign in
-              </Button>
+            <div className={`input-group mb-3 ${error ? 'has-error' : ''}`}>
+              <i className="input-group-text bi bi-key" />
+              <input
+                type={showPassword ? 'text' : 'password'}
+                className="form-control"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button
+                type="button"
+                className={`input-group-text bi bi-eye${showPassword ? '-slash' : ''}`}
+                onClick={() => setShowPassword(!showPassword)}
+              />
+              {error && <span className="help-block">{errorMessage}</span>}
             </div>
+            <div className={`input-group mb-3 ${error ? 'has-error' : ''}`}>
+              <i className="input-group-text bi bi-key" />
+              <input
+                type={showConfirmPassword ? 'text' : 'password'}
+                className="form-control"
+                placeholder="Confirm password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <button
+                type="button"
+                className={`input-group-text bi bi-eye${showConfirmPassword ? '-slash' : ''}`}
+                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+              />
+              {error && <span className="help-block">{errorMessage}</span>}
+            </div>
+            {success && (
+              <div className="form-group">
+                <div className="alert alert-success" role="alert">
+                  Login success
+                </div>
+              </div>
+            )}
+          </div>
+          <div className="modal-footer d-flex justify-content-around">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-target="#modalLogin"
+              data-bs-toggle="modal"
+              data-bs-dismiss="modal"
+            >
+              Login
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={handleSubmit}
+              disabled={loading}
+            >
+              Sign in
+            </button>
           </div>
         </div>
       </div>
-    </Form>
+    </div>
   );
 }
 
