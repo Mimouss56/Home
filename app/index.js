76f390680fb0bc7a7e35e0f5a0b740118421a5f5
Mouss <lepriol.matthieu@gmail.com>
2023-12-05 15:30:23 +0100

    fix method option

--- app/index.js
+++ app/index.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-Token');
-
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   // response to preflight request
   if (req.method === 'OPTIONS') {
     res.sendStatus(200);
