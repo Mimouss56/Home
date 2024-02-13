f8b18551f97356289d40d7c5ec884d6de38de1ff
Mouss <lepriol.matthieu@gmail.com>
2024-02-13 13:33:11 +0100

    Modification des termes pour les events des bots

---
+++ bot/bots/mimouss/events/quoioubeh.js
@@ -0,0 +1,15 @@
+const { Events } = require('discord.js');
+
+const regexQuoi = /\w+\s+quoi\??$/;
+module.exports = {
+  name: Events.MessageCreate,
+  async execute(message) {
+    const { author } = message;
+    if (regexQuoi.test(message.content) && !author.bot) { 
+      message.reply(`Hey tu sais quoi !?!?! <@${author.id}>`);
+      setTimeout(() => {
+        message.channel.send('QUOICOUBEH ! ! !');
+      }, 3 * 1000);
+    }
+  },
+};
