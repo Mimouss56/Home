46a5219745eb9f99edf7378800331803e3ee4e86
Mouss <lepriol.matthieu@gmail.com>
2024-02-20 14:52:45 +0100

    Mise à jour de la couleur de fond des lignes en fonction de la date de création des interactions

--- src/Pages/Emploi/listingInter.tsx
+++ src/Pages/Emploi/listingInter.tsx
@@ -1,6 +1,11 @@
 import dayjs from 'dayjs';
 import { IInterVue } from '../../@types/Home/ent';
 
+const objectColor = [
+  { color: 'table-danger', date: 15 },
+  { color: 'table-warning', date: 7 },
+  { color: 'table-success', date: 0 },
+];
 function ListInterations({ interactions }: { interactions: IInterVue[] }) {
   if (interactions.length === 0) {
     return <div>Aucune interation actuellementt</div>;
@@ -15,7 +20,7 @@ function ListInterations({ interactions }: { interactions: IInterVue[] }) {
         className="table-responsive"
       >
         <table
-          className="table table-primary"
+          className="table"
         >
           <thead>
             <tr>
@@ -29,13 +34,19 @@ function ListInterations({ interactions }: { interactions: IInterVue[] }) {
             {interactions
               .sort((a, b) => (a.createdAt > b.createdAt ? 1 : -1))
               .map((inter) => {
+                const diff = dayjs().diff(inter.createdAt, 'days');
+                const bgColor = objectColor.find((obj) => diff >= obj.date) || { color: 'table-info' };
+
                 // si created_at est > 7 jours, on l'affiche sinon on passe
-                if (dayjs().diff(dayjs(inter.createdAt), 'day') < 7) {
+                if (diff < 7) {
                   return null;
                 }
                 return (
                   (
-                    <tr key={inter.id}>
+                    <tr
+                      key={inter.id}
+                      className={bgColor.color}
+                    >
                       <td>{inter.entreprise}</td>
                       <td>{inter.contact}</td>
                       <td>{inter.status}</td>
