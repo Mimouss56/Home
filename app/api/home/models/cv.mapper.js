c6af80b6f6ac5efc5b7d77ba606bd837e9e2b8a7
Le Priol Matthieu <lepriol.matthieu@gmail.com>
2024-03-01 11:02:37 +0100

    Mise à jour de la requête SQL pour récupérer les informations du CV par l'ID utilisateur

--- app/api/home/models/cv.mapper.js
+++ app/api/home/models/cv.mapper.js
@@ -12,12 +12,14 @@ module.exports = class CV extends CoreDatamapper {
   async findAllByUserId(id) {
     const preparedQuery = {
       text: `
-        SELECT * FROM "${this.tableName}" 
-        INNER JOIN "${this.userRelated}" ON "${this.tableName}".id = "${this.userRelated}".id_job
-        INNER JOIN "${this.entTable}" ON "${this.entTable}".id = "${this.tableName}".id_ent
-        WHERE "${this.userRelated}".id_user = $1`,
+      SELECT cv.*, ent.name, ent.address, ent.town, ent.postal_code, ent.url_img
+      FROM "${this.tableName}"
+      INNER JOIN "${this.userRelated}" ON "${this.tableName}".id = "${this.userRelated}".id_job
+      INNER JOIN "${this.entTable}" ON "${this.entTable}".id = "${this.tableName}".id_ent
+      WHERE "${this.userRelated}".id_user = $1`,
       values: [id],
     };
+
     const result = await this.client.query(preparedQuery);
     return result.rows;
   }
