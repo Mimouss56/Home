b45d9012f137ba7b5e802a6f29031aa7c85911d4
Mouss <lepriol.matthieu@gmail.com>
2024-03-30 17:40:45 +0100

    Mise à jour des références de table dans hardSkill.sql

--- data/migration/deploy/hardSkill.sql
+++ data/migration/deploy/hardSkill.sql
@@ -7,13 +7,13 @@ CREATE TABLE IF NOT EXISTS hard_skill (
     label VARCHAR(255) NOT NULL,
     color VARCHAR(15) DEFAULT 'silver',
     url_icon VARCHAR(255) DEFAULT NULL,
-    stack VARCHAR(8) DEFAULT 'AUTRE' NOT NULL,
+    stack VARCHAR(8) DEFAULT 'AUTRE' NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS user_hard_skill (
     id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL REFERENCES home.user(id),
-    hard_skill_id INT NOT NULL REFERENCES home.hard_skill(id),
+    user_id INT NOT NULL REFERENCES public.user(id),
+    hard_skill_id INT NOT NULL REFERENCES hard_skill(id),
     level INT DEFAULT 0
 );
 
