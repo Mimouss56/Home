3dd0a283dd98cf7c38b3d5f3471906b9d36f95fe
Mouss <lepriol.matthieu@gmail.com>
2024-04-03 15:38:49 +0200

    Ajouter un formulaire de news modal dans la section Actualités

--- src/Pages/Main_2/news/index.tsx
+++ src/Pages/Main_2/news/index.tsx
@@ -1,5 +1,6 @@
 import { ICardNews } from '../../../@types/Home/card';
 import Card from '../../../components/Card';
+import ModalAddNews from '../../../components/Modal/News/formNews';
 import useScrollSection from '../../../hook/useScrollSection';
 import SectionLayout from '../../../layout/SectionLayout';
 
@@ -8,16 +9,19 @@ const idName = 'news';
 function NewsSection({ listNews }: { listNews: ICardNews[] }) {
   useScrollSection(idName);
   return (
-    <SectionLayout idName={idName} title="Actualités" addButton={null}>
-      <div className="d-flex flex-wrap w-75 mx-auto">
-        {listNews && listNews.map((item: ICardNews) => (
-          <Card key={item.id}>
-            {/* eslint-disable-next-line react/no-danger */}
-            <div dangerouslySetInnerHTML={{ __html: item.content }} />
-          </Card>
-        ))}
-      </div>
-    </SectionLayout>
+    <>
+      <SectionLayout idName={idName} title="Actualités" addButton="addModalNews">
+        <div className="d-flex flex-wrap w-75 mx-auto">
+          {listNews && listNews.map((item: ICardNews) => (
+            <Card key={item.id}>
+              {/* eslint-disable-next-line react/no-danger */}
+              <div dangerouslySetInnerHTML={{ __html: item.content }} />
+            </Card>
+          ))}
+        </div>
+      </SectionLayout>
+      <ModalAddNews onAddElement={() => { }} />
+    </>
 
   );
 }
