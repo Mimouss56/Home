9dcc6d2f7991e0c7c51fef1d12f9286ab1cdc80a
Mouss <lepriol.matthieu@gmail.com>
2024-04-03 22:53:43 +0200

    Mise à jour du style de la page de présentation et de la page de construction

--- src/Pages/Main_2/prez/index.tsx
+++ src/Pages/Main_2/prez/index.tsx
@@ -13,16 +13,12 @@ function Prez() {
 
   return (
     <div
-      className="vh-100 intro bg-dark"
+      className="vh-100 bg-dark"
       id="home"
-      style={{
-        background: `
-      radial-gradient(circle at 0 0, rgba($dark, $alpha), rgba($light, 1 - $alpha)),
-      radial-gradient(circle at 100% 0, rgba($dark, $alpha), rgba($light, 1 - $alpha))
-      `,
-      }}
     >
       <div className="circle" />
+      {/* <img src="https://www.mimouss.fr/images/2024/4/1/log_mimouss.png" alt="LogoSite" width={100} />
+      </div> */}
       <section id="landing-page" className="bg-dark vh-100 d-flex align-items-center ">
         <div className="w-75 h-50 m-auto row">
           <div className="col-md-6">
