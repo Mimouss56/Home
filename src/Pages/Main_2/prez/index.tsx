3f873a688be32e9bada617aa7eae736f171da42f
Mouss <lepriol.matthieu@gmail.com>
2024-04-18 14:34:04 +0200

    Mise à jour du service hardSkill pour utiliser 'hardSkill' au lieu de 'softSkill' dans le fichier hardSkill.service.js et mise à jour de la mise en page de la page principale et de la feuille de style dans le fichier index.tsx.

--- src/Pages/Main_2/prez/index.tsx
+++ src/Pages/Main_2/prez/index.tsx
@@ -6,70 +6,70 @@ import './borderNeon.scss';
 import '../../../scss/styles.scss';
 import { MoussID } from '../../../../config.json';
 import CarouselSkill from '../skill/carousel';
+import useScrollSection from '../../../hook/useScrollSection';
+import { IHard } from '../../../@types/Home/hardSkill';
+
+const idName = 'landing-page';
 
 const width = 300;
 function Prez() {
   const [dataMouss] = useFetchData(`/api/home/user/${MoussID}`);
   const [dataHardSkill] = useFetchData('/api/home/hardskill');
+  const listHardSkill = dataHardSkill.filter((skill: IHard) => skill.stack === 'back');
+
   const Mouss = dataMouss.user as IUser;
+  useScrollSection(idName);
 
   if (!Mouss) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div
-      className="vh-100 bg-dark"
-      id="home"
-    >
-      {/* <div className="circle" /> */}
-      <section id="landing-page" className="vh-100 d-flex align-items-center mb-5 position-relative overflow-hidden">
-        <div className="h-lg-50 m-md-auto row my-sm-0 h-sm-100">
-          <div className="col-md-6 my-auto px-5">
-            <h1 id="prezName" className="neonText">
-              l
-              <span className="blinkText">e</span>
-              {' '}
-              pr
-              <span className="blinkText">io</span>
-              l
-              {' '}
-              m
-              <span className="blinkText">a</span>
-              tth
-              <span className="blinkText">ieu</span>
-            </h1>
-            <p>{`${Mouss.prez}`}</p>
-            <CarouselSkill skills={dataHardSkill} />
-          </div>
-          <div className="col-md-6 d-flex justify-content-center my-auto mw-100">
-            <div
-              className="box position-relative "
-              style={{
-                width,
-                height: width,
-              }}
-            >
-              <span className="position-absolute top-0 h-100" />
-              <span className="position-absolute top-0 h-100" />
-              <span className="position-absolute top-0 h-100" />
-              <span className="position-absolute top-0 h-100" />
-              <span className="position-absolute top-0 h-100" />
-              <span className="position-absolute top-0 h-100" />
-              <img
-                src="https://www.mimouss.fr/images/2024/4/1/log_mimouss.png"
-                alt="Mouss"
-                width={width - 100}
-                height={width - 100}
-                className="position-absolute top-50 start-50 translate-middle rounded-circle "
-              />
-
-            </div>
-          </div>
-
+    <section id="landing-page" className="vh-100 d-flex align-items-center position-relative overflow-hidden bg-dark">
+      <div className="h-lg-50 m-md-auto row my-sm-0 h-sm-100">
+        <div className="col-md-6 my-auto px-5">
+          <h1 id="prezName" className="neonText">
+            l
+            <span className="blinkText">e</span>
+            {' '}
+            pr
+            <span className="blinkText">io</span>
+            l
+            {' '}
+            m
+            <span className="blinkText">a</span>
+            tth
+            <span className="blinkText">ieu</span>
+          </h1>
+          <p>{`${Mouss.prez}`}</p>
+          <CarouselSkill skills={dataHardSkill} />
         </div>
-      </section>
-    </div>
+        <div className="col-md-6 d-flex justify-content-center my-auto mw-100">
+          <div
+            className="box position-relative "
+            style={{
+              width,
+              height: width,
+            }}
+          >
+            {Array.from({ length: 6 }).map((_, i) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <span key={i} className="position-absolute top-0 h-100" />
+            ))}
+            <img
+              src="https://www.mimouss.fr/images/2024/4/1/log_mimouss.png"
+              alt="Mouss"
+              width={width - 100}
+              height={width - 100}
+              className="position-absolute top-50 start-50 translate-middle rounded-circle "
+            />
+
+          </div>
+        </div>
+
+      </div>
+    </section>
+
   );
 }
 
--- src/Pages/Main_2/skill/test.tsx
+++ src/Pages/Main_2/skill/test.tsx
@@ -3,36 +3,21 @@ import { IHard } from '../../../@types/Home/hardSkill';
 import Tags from '../../../components/Tag';
 
 function CarouselSkill({ skills }: { skills: IHard[] }) {
-  const [elementDisplayed, setElementDisplayed] = useState(1);
-  const marqueeRef = useRef<HTMLDivElement>(null);
-
-  const marqueeWidth = 50;
+  const [elementDisplayed, setElementDisplayed] = useState(5);
   const animationDuration = `${skills.length * 3}s`;
 
-  const style = document.createElement('style');
-  style.innerHTML = `
-    @keyframes scrolling {
-      0% { transform: translateX(0); }
-      100% { transform: translateX(${-1 * (marqueeWidth / elementDisplayed) * skills.length}vw); }
-    }
-  `;
-  document.head.appendChild(style);
+  const marqueeRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    // Calculer dynamiquement le nombre d'éléments affichés (elementDisplayed) en fonction de la largeur de défilement (marqueeWidth)
     if (marqueeRef.current) {
       const parentWidth = marqueeRef.current.clientWidth;
-
-      const itemWidth = 100; // Largeur de chaque élément (en %)
-      const displayedElements = Math.floor(parentWidth / itemWidth);
+      const itemWidth = 50; // Largeur de chaque élément (en %)
+      const totalWidth = itemWidth * skills.length;
+      const displayedElements = Math.floor(parentWidth / (totalWidth / skills.length));
       setElementDisplayed(displayedElements);
     }
-    const marqueeContent = document.querySelector('ul#marquee-content') as HTMLUListElement;
-
-    for (let i = 0; i < Number(elementDisplayed); i += 1) {
-      marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
-    }
-  }, [skills, elementDisplayed]);
-  if (!skills) return null;
+  }, [skills]);
 
   return (
     <div className="d-flex justify-content-center">
@@ -60,7 +45,6 @@ function CarouselSkill({ skills }: { skills: IHard[] }) {
               style={{ width: `${100 / elementDisplayed}%` }}
             >
               <Tags
-                // key={skill.id}
                 icon={skill.urlIcon}
                 name={skill.label}
                 color={skill.color}
@@ -68,25 +52,24 @@ function CarouselSkill({ skills }: { skills: IHard[] }) {
             </li>
           ))}
         </ul>
-        {/* <div
+        <div
           id="marqueeBefore"
           className="position-absolute top-0 h-100 z-1 start-0"
           style={{
-            // width: '10rem',
+            width: '10rem',
             content: '',
-            background: 'linear-gradient(to right, #203c4f 0%, transparent 100%)',
+            background: 'linear-gradient(to right, #111 0%, transparent 100%)',
           }}
         />
         <div
           id="marqueeAfter"
           className="position-absolute top-0 h-100 z-1 end-0"
           style={{
-            // width: '10rem',
+            width: '10rem',
             content: '',
-            background: 'linear-gradient(to left, #253e52 0%, transparent 100%)',
+            background: 'linear-gradient(to left, #111 0%, transparent 100%)',
           }}
-        /> */}
-
+        />
       </div>
     </div>
   );
