caf5a63e0e8239f3e7520a822abcf280de32c97c
Mouss <lepriol.matthieu@gmail.com>
2024-11-19 18:16:23 +0100

    Refactor middleware to use Promise.all for bot login and update AccueilPage to support dynamic URLs for projects

--- app/middlewares/bots.middleware.js
+++ app/middlewares/bots.middleware.js
@@ -4,14 +4,14 @@ const optionService = require('../api/home/services/option.service');
 
 const active = async () => {
   const allBot = await optionService.get({ where: { name: 'bot' } });
-  allBot.forEach(async (bot) => {
+  await Promise.all(allBot.map(async (bot) => {
     if (bot.active) {
       const botName = bot.value;
       const botToken = await optionService.getOne(botName);
       const botModule = require(`../../bot/bots/${botName}`);
       botModule.login(botToken.value);
     }
-  });
+  }));
 };
 
 module.exports = {
